80092e4372ada59bf0fa6a78b31b74e1abfcbde9
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-11-08 00:29:24 -0800

    Completed the Build a Next.js App Scrimba

---
+++ app/about/mission/page.jsx
@@ -0,0 +1,5 @@
+export default function Mission() {
+    return (
+        <h1>This is our mission at PrintForge...</h1>
+    )
+}
---
+++ app/components/ModelCard.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import Pill from "./Pill"
+import placeholderImg from "@/public/placeholder.png"
+
+export default function ModelCard({ model }) {
+    return (
+        <Link
+            href={`/3d-models/${model.id}`}
+            className="block group hover:shadow-[0_5px_12px_rgba(0,0,0,0.1)] hover:-translate-y-[3px] transition-all"
+            aria-labelledby={`model-${model.id}-title`}
+        >
+            <div className="overflow-hidden transition-shadow bg-white rounded-lg shadow-md hover:shadow-lg" role="article">
+                <div className="relative aspect-square">
+                    <img
+                        src={placeholderImg.src}
+                        alt={model.name}
+                        className="absolute inset-0 object-cover w-full h-full"
+                    />
+                </div>
+                <div className="p-4">
+                    <div className="flex justify-between mb-2 min-h-[3.5rem]">
+                        <h2 id={`model-${model.id}-title`} className="text-xl font-semibold text-gray-800 line-clamp-2">
+                            {model.name}
+                        </h2>
+                    </div>
+                    <p className="text-gray-800 text-sm line-clamp-2 min-h-[2.5rem] leading-[1.25rem]">
+                        {model.description}
+                    </p>
+                    <div className="mt-2">
+                        <Pill>{model.category}</Pill>
+                    </div>
+                    <div className="flex items-center mt-2 text-gray-600" aria-label={`${model.likes} likes`}>
+                        <span>{model.likes}</span>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    )
+}
